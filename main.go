8f0bd223ad06d69e5c164ebf0d9853ab9ec4788c
AccurateApplication <lucas@cederqvist.dev>
2022-04-30 11:55:18 +0200

    add template parsing

--- main.go
+++ main.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/about", aboutHandler)
 	http.HandleFunc("/", helloHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
 	fmt.Fprintf(w, "Hello")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.tmpl")
+	fmt.Fprintf(w, "this is about page")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		log.Println("Error parsing template: ", err)
+		return
+	}
 }
