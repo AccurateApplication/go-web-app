332ee42ae7ae22d5923447a98495965456336220
AccurateApplication <lucas@cederqvist.dev>
2022-05-03 14:57:31 +0200

    cleanup handlers.go functions

--- handlers.go
+++ handlers.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "home.page.tmpl")
-	fmt.Fprintf(w, "Hello")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "about.page.tmpl")
-	fmt.Fprintf(w, "this is about page")
 }
