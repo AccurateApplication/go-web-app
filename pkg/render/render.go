a9d2781b143a8283191d71488406961d8abec40a
AccurateApplication <lucas@cederqvist.dev>
2022-05-04 08:13:50 +0200

    Remove duplicate render functions

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,7 +1,6 @@
 package render
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -9,14 +8,6 @@ import (
 
 // RenderTemplate renders a template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template:", err)
-	}
-}
-
-func RenderTemplate1(w http.ResponseWriter, tmpl string) {
 	parsedTemplate, err := template.ParseFiles("./templates/" + tmpl)
 	if err != nil {
 		log.Println("Error parsing files in templates. err: ", err)
