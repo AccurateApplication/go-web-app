24b9584f1d5ae98bb867c42c0b0e3524e23577ab
AccurateApplication <lucas@cederqvist.dev>
2022-05-04 13:18:22 +0200

    Rewrite render.go to create a map of templates
    But it does this every time a page is loaded. Need to rewrite so that it
    does it one time only, at application start.

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,20 +1,79 @@
 package render
 
 import (
+	"bytes"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
+
+var funcMap = template.FuncMap{}
 
 // RenderTemplate renders a template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, err := template.ParseFiles("./templates/" + tmpl)
+	// TODO Fix - for now, a template cache every time a site is loaded, bad
+	tmplCache, err := CreateTmplCache()
 	if err != nil {
-		log.Println("Error parsing files in templates. err: ", err)
+		log.Fatalln("Error creating template cache: ", err)
 	}
-	err = parsedTemplate.Execute(w, nil)
+
+	t, ok := tmplCache[tmpl]
+	if !ok {
+		log.Fatalln("Could not find template, err: ", err)
+	}
+
+	buf := new(bytes.Buffer)
+
+	_ = t.Execute(buf, nil)
+	_, err = buf.WriteTo(w)
+
 	if err != nil {
-		log.Println("Error parsing template: ", err)
-		return
+		log.Println(err)
 	}
+}
+
+// Generate a cache as map with string and a pointer to template to render site with
+func CreateTmplCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	layoutPath := "./templates/*.layout.tmpl"
+	templatePath := "./templates/*.page.tmpl"
+
+	pages, err := filepath.Glob(templatePath)
+	if err != nil {
+		return cache, err
+	} else if len(pages) == 0 {
+		log.Fatalf("Found no templates with glob: %s\n", templatePath)
+	}
+
+	for _, page := range pages {
+		fmt.Println("After range over pages")
+		pageName := filepath.Base(page)
+		fmt.Println(pageName)
+
+		// Get template set.
+		ts, err := template.New(pageName).Funcs(funcMap).ParseFiles(page)
+		if err != nil {
+			return cache, err
+		}
+
+		matches, err := filepath.Glob(layoutPath)
+		fmt.Println(matches)
+		if err != nil {
+			return cache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err := ts.ParseGlob(layoutPath)
+			if err != nil {
+				return cache, err
+			}
+			cache[pageName] = ts
+		}
+		cache[pageName] = ts
+
+	}
+	return cache, nil
 }
