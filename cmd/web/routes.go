522facc9c69ce33bf985fd03f4fd8856c4c03f6b
AccurateApplication <lucas@cederqvist.dev>
2022-05-05 08:03:26 +0200

    add routes.go, that uses gorilla mux

---
+++ cmd/web/routes.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/AccurateApplication/go-web-app/pkg/config"
+	"github.com/AccurateApplication/go-web-app/pkg/handlers"
+	"github.com/gorilla/mux"
+)
+
+func routes(conf *config.AppConf) http.Handler {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handlers.Repo.HelloHandler)
+	r.HandleFunc("/about", handlers.Repo.AboutHandler)
+	return r
+}
